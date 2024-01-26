0e9021e93478096c661a789244ef7652dc7b7520
jpedro002 <joaopsilvavolei@gmail.com>
2024-01-26 16:10:21 -0300

    feat: deatail product page

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,58 @@
+import { ImageResponse } from 'next/og'
+import colors from 'tailwindcss/colors'
+
+import { ENV } from '@/env'
+import { Product } from '../../(home)/page'
+import { api } from '@/app/services/api'
+
+export const runtime = 'edge'
+
+export const alt = ''
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 15, // 15 minutes
+    },
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+export default async function OgImage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await getProduct(params.slug)
+
+  const productImageURL = new URL(product.image, ENV.APP_URL).toString()
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={productImageURL} alt="" style={{ width: '100%' }} />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
